afd1421c1fc290a4b566e22bba68180ac3188df5
jayjun <jayjun.ryu@kakaocorp.com>
2021-09-24 21:07:59 +0900

    혼자서 공부하는 파이썬 책 테스트 코드 연습

---
+++ chapter1/ex01/comment.py
@@ -0,0 +1,2 @@
+#간단히 출력하는 예입니다.
+print("Hello Python Programming...") #문자열을 출력 합니다.
---
+++ chapter1/ex01/print.py
@@ -0,0 +1,12 @@
+# 하나만 출력
+# print("Hello Python Programming...")
+# print(52)
+# print(273)
+
+# 여러개 출력
+# print(52, 273, "Hello")
+# print("안녕하세요","저의", "이름은","제이준 입니다!")
+
+print("안녕")
+print()
+print("줄 바꿈후 출력")
