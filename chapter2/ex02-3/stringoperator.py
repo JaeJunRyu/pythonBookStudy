03b796d24e61a2ebb077ef53529cf10341e995c4
jayjun <jayjun.ryu@kakaocorp.com>
2021-10-03 14:04:25 +0900

    혼자서 공부하는 파이썬 책 챕터2 : 숫자타입

---
+++ chapter2/ex02-3/stringoperator.py
@@ -0,0 +1,8 @@
+string="안녕하세요"
+string+="!"
+string+="!"
+print("string: ", string)
+
+string1 = "안녕하세요"
+string1 *= 3
+print("string1: ", string1)
---
+++ chapter2/ex02-3/variable.py
@@ -0,0 +1,25 @@
+pi=3.14159265
+print(pi)
+print()
+print(pi + 2)
+print()
+print(pi - 2)
+print()
+print(pi * 2)
+print()
+print(pi / 2)
+print()
+print(pi % 2)
+print()
+print(pi + pi)
+
+print()
+#변수 선언과 할당
+pi1=3.14159265
+r=10
+
+#변수 참조
+print("원주율 = ", pi1)
+print("반지름 = ", r)
+print("원의 둘레 = ", 2 * pi1 * r)
+print("원의 넓이 = ", pi1 * r * r)
